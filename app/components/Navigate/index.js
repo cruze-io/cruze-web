92b15d66cd7dd378d90ef5404fb5bb987a6f1a3f
Yash Saxena <yash.saxena1217@gmail.com>
2017-02-12 18:59:46 -0800

    test new heading calculation

--- app/components/Navigate/index.js
+++ app/components/Navigate/index.js
@@ -123,8 +123,39 @@ class Navigate extends Component {
   }
   startTrackingUserHeading() {
     const handleOrientation = (event) => {
-      if (event && event.alpha) {
-        this.map.rotateTo(event.alpha)
+      if (event && event.alpha && event.beta && event.gamma) {
+        // Convert degrees to radians
+        var alphaRad = event.alpha * (Math.PI / 180);
+        var betaRad = event.beta * (Math.PI / 180);
+        var gammaRad = event.gamma * (Math.PI / 180);
+
+        // Calculate equation components
+        var cA = Math.cos(alphaRad);
+        var sA = Math.sin(alphaRad);
+        var cB = Math.cos(betaRad);
+        var sB = Math.sin(betaRad);
+        var cG = Math.cos(gammaRad);
+        var sG = Math.sin(gammaRad);
+
+        // Calculate A, B, C rotation components
+        var rA = - cA * sG - sA * sB * cG;
+        var rB = - sA * sG + cA * sB * cG;
+        var rC = - cB * cG;
+
+        // Calculate compass heading
+        var compassHeading = Math.atan(rA / rB);
+
+        // Convert from half unit circle to whole unit circle
+        if(rB < 0) {
+          compassHeading += Math.PI;
+        }else if(rA < 0) {
+          compassHeading += 2 * Math.PI;
+        }
+
+        // Convert radians to degrees
+        compassHeading *= 180 / Math.PI;
+
+        this.map.rotateTo(compassHeading)
       }
     }
     window.addEventListener("deviceorientation", handleOrientation, true);
